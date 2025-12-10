209270ae62603b4909e8c3664ca67ad15ee47099
damali-pde <damaliprince69@gmail.com>
2025-12-10 16:45:50 +0000

    Add playlist and music management in Python

---
+++ basics of python.py
@@ -0,0 +1,22 @@
+#create a list called playlist containing at least 5 song titles
+playlist = ["healer","normally","propeller","bye","welcome"]
+print(playlist)
+
+#create a tuple called top artists
+top_artists =("blacko","bnxn","drake","mavo") 
+print(top_artists)
+
+#create a new list called my music
+my_music = ["playlist","top_artiste"]
+print(my_music)
+
+#print the third song from playlist
+print(playlist[2]) 
+
+#change the first song in playlist to "perfect"
+playlist[0]= "Perfect"
+print(playlist)
+
+#add the song "blinding lights to the end of the playlist"
+playlist.insert(5,"blinding lights")
+print(playlist)
