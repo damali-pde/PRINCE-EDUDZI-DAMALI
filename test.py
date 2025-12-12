13806c9d13adfb7721d3b92828582d2a369c6b86
damali-pde <damaliprince69@gmail.com>
2025-12-12 14:54:02 +0000

    Comment out age-based movie recommendation logic

---
+++ test.py
@@ -0,0 +1,9 @@
+#age = int(input(" enter your age:"))
+ #if age >= 18:
+  #   print("you can watch any movie(including Adult rated movies).")
+# elif age >= 13:
+  #   print("you can watch movies rated pg-13 and lower-rated movies.")
+ #elif age >=7:
+    # print("you can watch G and PG- rated movies.")
+ #else:
+   #  print("you should watch only g-rated (general audience) movies.")
